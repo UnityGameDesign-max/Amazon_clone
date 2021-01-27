2401c2db88a54914651a942dbb98a6b6009e7331
Tumelo <tumie201511@gmail.com>
2021-01-27 15:29:02 +0200

    added Header js file for the navigatiion bar

---
+++ frontend/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search';
+
+function Header() {
+    return (
+        <div>
+            <h1>Programming amazon clone </h1>
+            <SearchIcon />
+        </div>
+    )
+}
+
+export default Header
