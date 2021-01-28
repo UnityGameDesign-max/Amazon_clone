da292bac38f35a61148ca4fdf221df6e878222d5
Tumelo <tumie201511@gmail.com>
2021-01-28 03:44:11 +0200

    updated Header.js file

--- frontend/src/Header.js
+++ frontend/src/Header.js
@@ -4,9 +4,14 @@ import SearchIcon from '@material-ui/icons/Search';
 
 function Header() {
     return (
-        <div>
-            <h1>Programming amazon clone </h1>
-            <SearchIcon />
+        <div className="header">
+            <img className="header_logo" alt="amazon_img" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"/>
+
+            <div className="header_search">
+                <input className="header_search_input" type="text" />
+                <SearchIcon className="header_searchIcon"/>
+            </div>
+            
         </div>
     )
 }
