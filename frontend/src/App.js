0846e1efe86333e29bbf836b04154222da12542c
Tumelo <tumie201511@gmail.com>
2021-01-28 13:22:39 +0200

    updated main App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,24 @@ import React from 'react'
 import './App.css';
 import Header from './Header'
 import SubHeader from './SubHeader'
-
+import Home from './Home'
+import Products from './Products'
 
 function App() {
   return (
     <div className="app">
       <Header />
       <SubHeader />
+      <Home />
+
+      <div className="catergory_row">
+        <Products />
+        <Products />
+      </div>
+
+      <div className="catergory_row">
+
+      </div>
     </div>
   );
 }
