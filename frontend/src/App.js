8405b378c0574eaf230fce2cd7a1995da5103447
Tumelo <tumie201511@gmail.com>
2021-01-28 03:42:39 +0200

    updated App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css';
 import Header from './Header'
+import SubHeader from './SubHeader'
 
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <SubHeader />
     </div>
   );
 }
