3e4ca98bdd50366beadc5a2db9ec28edc78541c6
Tumelo <tumie201511@gmail.com>
2021-01-28 13:26:32 +0200

    updated Home js file

---
+++ frontend/src/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home_container">
+                <img className="home_image" alt="" src="https://images.squarespace-cdn.com/content/554bc4b3e4b06fc215139327/1436374566265-W0XK9SJKZ7KSJFYSQL47/image-asset.jpeg?format=1000w&content-type=image%2Fjpeg" />
+            </div>
+            
+        </div>
+    )
+}
+
+export default Home
