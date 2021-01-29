4f686af495964ab77bca958ba805d74965cac2a9
Tumelo <tumie201511@gmail.com>
2021-01-29 12:49:28 +0200

    added a new App js file for the main project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
 import React from 'react'
 import './App.css';
 import Header from './Header'
-import SubHeader from './SubHeader'
 import Home from './Home'
-import Products from './Products'
+
 
 function App() {
   return (
     <div className="app">
       <Header />
-      <SubHeader />
       <Home />
-
-      <div className="catergory_row">
-        <Products />
-        <Products />
-      </div>
-
-      <div className="catergory_row">
-
-      </div>
     </div>
   );
 }
