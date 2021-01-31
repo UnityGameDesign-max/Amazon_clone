8ddc0b78a9b033326513ed1d27f945cdfabe12bb
Tumelo <tumie201511@gmail.com>
2021-01-31 16:10:26 +0200

    modified App js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,22 +2,36 @@ import React from 'react'
 import './App.css';
 import Header from './Header'
 import Home from './Home'
-import {BrowserRouter as Router, Switch, Route} from'react-router-dom'
+import {BrowserRouter as Router, Switch, Route}
+from'react-router-dom'
+import Deals from './Deals'
+import Login from './Login'
 
 
 function App() {
   return (
     <Router>
       <div className="app">
+       
         <Switch>
+
+          <Route path="/login">
+            <Login />
+
+          </Route>
+          <Route path="/deals">
+            <Header />
+            <Deals />
+          </Route>
+
           <Route path="/">
             <Header />
             <Home />
           </Route>
+
         </Switch>
       </div>
     </Router>
-    
   );
 }
 
