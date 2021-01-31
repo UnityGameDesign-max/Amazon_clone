e1cf37cb7296dada8391e448dc20dde16b9d06e4
Tumelo <tumie201511@gmail.com>
2021-01-31 16:07:59 +0200

    added Deals js file

---
+++ frontend/src/Deals.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Deals.css'
+
+function Deals() {
+    return (
+        <div className="all_products">
+            <h1>Deals and Promotions</h1>
+            <p>Shop Today's Deals, Lightning Deals,and Limited-time discounts</p>
+        </div>
+    )
+}
+
+export default Deals
