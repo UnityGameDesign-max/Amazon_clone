31ee6ab0f96a2ef98ece46203bfdf7368d4af601
Tumelo <tumie201511@gmail.com>
2021-01-31 16:14:40 +0200

    updated Products js file

---
+++ frontend/src/Products.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Products.css'
+
+function Products() {
+    return (
+        <div className="products">
+            
+        </div>
+    )
+}
+
+
+export default Products
