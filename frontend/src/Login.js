eac2d80331b82896d82017ad0f148ed4eab9507f
Tumelo <tumie201511@gmail.com>
2021-02-01 14:00:04 +0200

    updated Login js file

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,10 +1,42 @@
-import { Link } from 'react-router-dom'
-import React from 'react'
+import { Link, useHistory } from 'react-router-dom'
+import React, {useState} from 'react'
 import './Login.css'
+import { auth } from './firebase';
 
 
 
 function Login() {
+    
+    const history = useHistory();
+    // variables that placed as containers to put input made by the user
+    // email = e => SetEmail(e.target.value)
+    // password = e => SetPassword(e.target.value)
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+
+    //sigIn function that allow us to use email and password that was inserted from the user input
+    // And checks if the user exists from the firebase db
+    const signIn = e =>{
+        e.preventDefault();
+
+        auth.signInWithEmailAndPassword(email, password).then((auth) =>{
+            history.push("/")
+        })
+        .catch(error => alert(error.message))
+    }
+
+
+    const register = e =>{
+        e.preventDefault();
+        auth.createUserWithEmailAndPassword(email, password).then((auth) =>{
+            if(auth){
+                history.push("/")
+            }
+        })
+        .catch(error => alert(error.message)) 
+    }
+
     return (
         <div className="login">
             <Link to='/'>
@@ -15,24 +47,30 @@ function Login() {
                 <h1>Sign-in</h1>
 
                 <form>
-                    <h5>E-mail</h5>
-                    <input type="text" />
+                     <h5>E-mail</h5>
+                    <input type="text" value={email} onChange={e => setEmail(e.target.value)} />
 
                     <h5>Password</h5>
-                    <input type="password" />
+                    <input type="password" value={password}  onChange={e => setPassword(e.target.value)}  />
 
-                    <button className="login_signInBtn">Sign In</button>
+                    <button type="submit" className="login_signInBtn" onClick={signIn}>Sign In</button>
                 </form>
 
-                <p>By signing-in you agree to the AMAZON FAKE CLONE Conditions
-                    of Use & Sale. Please see our Privacy Notice, our Cookies Notice and
-                    our Interest-Based Ads Notice.
+                <p>
+                    <strong>Disclaimer</strong>: This is a fake Amazon Clone created by Tumelo Mahlangu and Lehlohonolo Mopeli
+                    to showcase their ReactJs skills. This web application has full user authentication(you can register and gain access).
+                    If you just want to test, use the following login details:
+                    
                 </p>
 
-                <button className="Login_registrationBtn">Create your Amazon Account</button>
+                <span className="email"><strong>Email : guest@gmail.com</strong></span>
+                <span className="password"><strong>Password : 123456</strong></span> 
+
+
+
+                <button className="Login_registrationBtn" onClick={register}>Create your Amazon Account</button>
             </div>
         </div>
-
       
     )
 }
