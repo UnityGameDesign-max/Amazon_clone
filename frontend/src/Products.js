6257a97bc92a3445c362e7f50e06cfd9a1dbf7b7
Tumelo <tumie201511@gmail.com>
2021-02-02 14:25:39 +0200

    modified Products js file

--- frontend/src/Products.js
+++ frontend/src/Products.js
@@ -1,10 +1,33 @@
+import { Link } from '@material-ui/core'
 import React from 'react'
 import './Products.css'
 
-function Products() {
+
+function Products({product_image, price_dollar, price_cent, product_description, specials}) {
     return (
         <div className="products">
             
+            <div className="product_info">
+                <img alt="photos" className="product_img" src={process.env.PUBLIC_URL + product_image} />
+                <p>{specials}</p>
+                <span>
+                    $
+                    <span className="price_dollar">
+                        {price_dollar}
+                    </span>
+                    <sup className="price_cent">
+                        {price_cent}
+                    </sup>
+                </span>
+                <Link>
+                    <span className="product_description">{product_description}</span>
+                </Link>
+
+                <span></span>
+
+                <button className="cart-btn">Add to Cart</button>
+                
+            </div>
         </div>
     )
 }
