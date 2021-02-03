c16ef1d751ec9cd57df8de69c1382d40dbe01b64
Tumelo <tumie201511@gmail.com>
2021-02-03 13:57:29 +0200

    updated App js file

--- frontend/src/App/App.js
+++ frontend/src/App/App.js
@@ -7,6 +7,8 @@ from'react-router-dom'
 import Deals from '../Deals/Deals'
 import Footer from '../Footer/Footer'
 import Login from '../Login/Login'
+import Checkout from '../Checkout/Checkout'
+import Subtotal from '../Subtotal/Subtotal'
 
 
 function App() {
@@ -19,12 +21,17 @@ function App() {
   
           <Route path="/login">
             <Login />
-
           </Route>
+          
           <Route path="/deals">
             <Header />
             <Deals />
             <Footer />
+          </Route>
+
+          <Route path="/checkout">
+            <Header /> 
+            <Checkout />
           </Route>
 
           <Route path="/">
