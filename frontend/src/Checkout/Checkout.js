4fd705a1b4583e9514ae838d355147c039816f0c
Tumelo <tumie201511@gmail.com>
2021-02-04 16:17:26 +0200

    modified Checkout js file

--- frontend/src/Checkout/Checkout.js
+++ frontend/src/Checkout/Checkout.js
@@ -1,21 +1,43 @@
 import React from 'react'
 import './Checkout.css'
 import Subtotal from '../Subtotal/Subtotal'
-
+import { useStateValue } from '../StateProvider/StateProvider'
+import CheckoutProduct from '../CheckoutProduct/CheckoutProduct'
+import { getBasketTotal } from '../Reducer/Reducer'
+ 
 
 function Checkout() {
+
+    const [{basket}, dispatch] = useStateValue();
+
+
     return (
         <div className="checkout">
             <div className="checkout_left">
                 <h1 className="checkout_left_title">Shopping Cart</h1>
                 <p className="checkout_pricer_border">Price</p>
 
-                <span>All Total products will go here!</span>
-            </div>      
+                {/* <CheckoutProduct product_image="/amazon_pics/gaming_headphones.jpg" price_dollar="122" price_cent= "23" product_description = "test checkout product" /> */}
+
+            
+                {basket.map(item =>(
+                    <CheckoutProduct 
+                    id= {item.id}
+                    product_image= {item.product_image}
+                    price_dollar= {item.price_dollar}
+                    price_cent= {item.price_cent}
+                    product_description= {item.product_description}
+                    />
+                ))}
+
+
+                <h1 className="checkout_left_subtotal">Subtotal({basket.length} items): $ {getBasketTotal(basket)} </h1>     
+            </div>
 
             <div className="checkout_right">
                 <Subtotal />    
-            </div> 
+            </div>
+            
         </div>
     )
 }
