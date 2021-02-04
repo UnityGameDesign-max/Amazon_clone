864ff2f9279acb74eb18505801792a98621b7b58
Tumelo <tumie201511@gmail.com>
2021-02-04 16:06:56 +0200

    updated a Reducer js file

--- frontend/src/Reducer/Reducer.js
+++ frontend/src/Reducer/Reducer.js
@@ -1,5 +1,6 @@
 export const initialState = {
     basket:[],
+    user: null
 
 };
 
@@ -12,6 +13,7 @@ export const getBasketTotal =(basket) =>
 
 
 const reducer = (state, action) =>{
+    
     switch(action.type){
         case 'ADD_TO_BASKET':
             return {
@@ -19,10 +21,34 @@ const reducer = (state, action) =>{
                 basket: [...state.basket, action.item],
 
             };
-        default:
+
+        case 'REMOVE_FROM_BASKET':
+
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+
+            let newBasket = [...state.basket];
+
+            if (index >= 0) {
+                newBasket.splice(index, 1);
+
+            }
+
+            else{
+                console.warn(`Cant remove product (id: ${action.id}) as its not in basket!`) 
+            }
+
+            return {
+                ...state,
+                basket: newBasket
+            }
+            
+
+        default: 
             return state; 
+
     }
 };
-
 
 export default reducer
