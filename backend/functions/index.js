82e732a31c35a21e612277a0fea0a3b8cf633703
LehlohonoloMopeli <lehlohonolompl@gmail.com>
2021-02-08 15:47:26 +0200

    Modified backend/functions/index.js

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -3,7 +3,7 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
-const stripe = require("stripe")()
+const stripe = require("stripe")('sk_test_51IGloHBskh1aSg7k3f2HDbCPVtEKBODnCVtQjuCs1TGBQhFpjKR9yw9F6Gjes9ms606vGPumRGVeDlszItGR2W1100HrsLZGJq')
 
 
 // API
@@ -22,7 +22,9 @@ app.use(express.json());            // Allows us to send data in json format
 app.get('/', (req, res) => res.status(200).send('Hello Express!'));
 
 app.post('/create_payments', async (req, res) => {
-    const total = req.query.total;
+    const total = req.query.Total;
+
+    console.log('Payment request of: ', total)
 
     const paymentIntent = await stripe.paymentIntents.create({
         amount: total,
