fbae082f8be8d80ee6aafc108a9e49bab3b1277f
Tumelo <tumie201511@gmail.com>
2021-02-12 14:17:33 +0200

    modified a Reducer js file

--- frontend/src/Reducer/Reducer.js
+++ frontend/src/Reducer/Reducer.js
@@ -49,6 +49,11 @@ const reducer = (state, action) =>{
                 ...state,
                 basket: newBasket
             }
+        case 'SET_USER':
+            return{
+                ...state,
+                user: action.user
+            }
             
 
         default: 
