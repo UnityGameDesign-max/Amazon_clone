8859397b4ab32d53eee828d31dba8ea64352ec07
Tumelo <tumie201511@gmail.com>
2021-02-26 10:35:10 +0200

    modified SignIn js file

--- frontend/src/SignIn/SignIn.js
+++ frontend/src/SignIn/SignIn.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './SignIn.css'
+import { useHistory } from 'react-router-dom'
 
 function SignIn({title, btn_text, image}) {
+
+    const history = useHistory();
+
+
+    const handle_signIn = () =>{
+        history.push("/login")    
+
+    }
+
     return (
         <div className='sign_in_catergories'>
             <div className='sign_in'>
                 <h2 className="card_header">{title}</h2>
-                <button className='sigin_btn'>{btn_text}</button>
+                <button className='sigin_btn' onClick={handle_signIn}>{btn_text}</button>
             </div>
             <div className="description_catergories">
                 <img alt="" src={image}/>
